e2d2a862e3bed8fa0943724463a5026b076d608f
Egevtech <egevtech@gmail.com>
2026-05-27 21:27:47 +0300

    add Option class

---
+++ src/main/java/com/github/owl_cult_devteam/owlboost/Option.java
@@ -0,0 +1,57 @@
+package com.github.owl_cult_devteam.owlboost;
+
+import com.github.owl_cult_devteam.owlboost.exceptions.*;
+
+import java.util.function.Supplier;
+
+@SuppressWarnings("unused")
+public sealed interface Option<T> {
+    record Some<T>(T t) implements Option<T> {
+        @Override
+        public T unwrap() throws UnwrapEmptyException {
+            return t;
+        }
+
+        @Override
+        public T unwrap_or(T _t) {
+            return t;
+        }
+
+        @Override
+        public T unwrap_or_null() {
+            return t;
+        }
+
+        @Override
+        public T unwrap_or_else(Supplier<T> _tSupplier) {
+            return t;
+        }
+    }
+
+    record None<T>() implements Option<T> {
+        @Override
+        public T unwrap() throws UnwrapEmptyException {
+            throw new UnwrapEmptyException("Trying to unwrap empty Option");
+        }
+
+        @Override
+        public T unwrap_or(T t) {
+            return t;
+        }
+
+        @Override
+        public T unwrap_or_null() {
+            return null;
+        }
+
+        @Override
+        public T unwrap_or_else(Supplier<T> tSupplier) {
+            return tSupplier.get();
+        }
+    }
+
+    T unwrap() throws UnwrapEmptyException;
+    T unwrap_or(T t);
+    T unwrap_or_null();
+    T unwrap_or_else(Supplier<T> t);
+}
