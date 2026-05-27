2427bc450271c97c819c9794bc4c2eb3de79c637
Egevtech <egevtech@gmail.com>
2026-05-27 21:27:39 +0300

    Add UnwrapEmptyException class

---
+++ src/main/java/com/github/owl_cult_devteam/owlboost/exceptions/UnwrapEmptyException.java
@@ -0,0 +1,7 @@
+package com.github.owl_cult_devteam.owlboost.exceptions;
+
+public class UnwrapEmptyException extends RuntimeException {
+    public UnwrapEmptyException(String message) {
+        super(message);
+    }
+}
